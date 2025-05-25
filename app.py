5deddc4fee47b527d3b7adb664758c610bd2d97a
Dix <admin@Dixons-MacBook-Pro.local>
2025-05-25 18:33:41 -0500

    App funcionando

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 from app_poke.routes import pokenea_bp
 import os
 
+# Configurar ruta explícita para templates y static
 app = Flask(
     __name__,
-    template_folder=os.path.join("app", "templates"),
-    static_folder=os.path.join("app", "static")
+    template_folder="app_poke/templates",
+    static_folder="app_poke/static"
 )
 
 app.register_blueprint(pokenea_bp, url_prefix="/pokenea")
@@ -15,4 +16,4 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
